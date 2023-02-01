d4821c178bd2a83a67142cb0dcd770930664ef01
StanislavPliev <verdigrisornot@gmail.com>
2023-02-01 17:55:55 +0300

    final result w/ adaptive & parcel & animations

---
+++ dist/index.579125c3.js
@@ -0,0 +1,17 @@
+const notificationNode = document.querySelector(".js-notif");
+const closeNotificationNode = document.querySelector(".js-close-notif");
+closeNotificationNode.addEventListener("click", function() {
+    notificationNode.style.animation = "close-notification 2s forwards";
+});
+const contentNode = document.querySelector(".js-content");
+const exitWindowNode = document.querySelector(".js-exit-window");
+const outputAfterExitNode = document.querySelector(".js-output-after-exit");
+exitWindowNode.addEventListener("click", function() {
+    contentNode.style.animation = "close-reg-window 1.5s forwards";
+    function afterExitText() {
+        outputAfterExitNode.innerHTML = "Перезагрузите страницу";
+    }
+    setTimeout(afterExitText, 800);
+});
+
+//# sourceMappingURL=index.579125c3.js.map
